bcbe81a6f34b1b6da502af3b383838936cfe03f9
Johan Vos <jvos@openjdk.org>
2026-09-10 11:36:03 +0000

    8389783: Crash in JavaFX native glass toolkit during Timer shutdown
    Backport-of: 5eaa16bf281d7bfeff7bd2e0cc7c500794aec123

--- modules/javafx.graphics/src/main/java/com/sun/javafx/tk/quantum/QuantumToolkit.java
+++ modules/javafx.graphics/src/main/java/com/sun/javafx/tk/quantum/QuantumToolkit.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -542,6 +542,16 @@ public final class QuantumToolkit extends Toolkit {
         pulseTimer.resume();
     }
 
+    private void stopTimer() {
+        // Stop the timer in a try/catch so that an IllegalStateException won't
+        // abort toolkit shutdown.
+        try {
+            pulseTimer.stop();
+        } catch (IllegalStateException ex) {
+            ex.printStackTrace();
+        }
+    }
+
     private String pulseString() {
         return ((toolkitRunning.get() ? "T" : "t") +
                 (animationRunning.get() ? "A" : "a") +
@@ -831,7 +841,7 @@ public final class QuantumToolkit extends Toolkit {
         checkFxUserThread();
 
         // Turn off pulses so no extraneous runnables are submitted
-        pulseTimer.stop();
+        stopTimer();
 
         // We need to wait for the last frame to finish so that the renderer
         // is not running while we are shutting down glass.
@@ -855,7 +865,7 @@ public final class QuantumToolkit extends Toolkit {
     @SuppressWarnings("removal")
     public void dispose() {
         if (toolkitRunning.compareAndSet(true, false)) {
-            pulseTimer.stop();
+            stopTimer();
             renderer.stopRenderer();
 
             try {
